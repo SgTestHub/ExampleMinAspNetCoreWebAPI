a0dec28aeeb252aaf71e7df6a329b2ae42c15573
Sheetal <gajare.sheetal@gmail.com>
2026-02-19 22:55:51 +1100

    added endpoints

--- Src/TestAPIs/Program.cs
+++ Src/TestAPIs/Program.cs
@@ -1,6 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using TestAPIs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+//Add DI - add sevrices
+builder.Services.AddDbContext<TestDB>(options =>
+    options.UseInMemoryDatabase("TestAPIListDB"));
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//Configure pipeline -UseMethod
+app.MapGet("/api/testitems", async (TestDB db) =>
+    await db.TestItem.ToListAsync());
+
+app.MapPost("/api/testitems", async (TestDB db, TestModelItem item) =>
+{
+    db.TestItem.Add(item);
+    await db.SaveChangesAsync();
+    return Results.Created($"/api/testitems/{item.Id}", item);
+});
 
 app.Run();
---
+++ Src/TestAPIs/TestDB.cs
@@ -0,0 +1,11 @@
+﻿using  Microsoft.EntityFrameworkCore;
+namespace TestAPIs
+{
+    public class TestDB:DbContext
+    {
+        public TestDB(DbContextOptions<TestDB> options) : base(options)
+        {
+        }
+        public DbSet<TestModelItem> TestItem { get; set; }
+    }
+}
